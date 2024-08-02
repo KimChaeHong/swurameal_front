54fc0fd3b0741e62104da370a46b64f33cfe72ff
361_seok <jws4858@gmail.com>
2024-08-02 16:21:42 +0900

    ✅ Add :" 마이페이지 ajax 기능 추가"
    mypage.js ajax로 파일 불러오기 기능 추가

--- js/mypage.js
+++ js/mypage.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    
+    $('a').on('click', function(e){
+        e.preventDefault();
+        let url = $(this).attr('href');
+
+        $.ajax({
+            url:url,
+            method: 'GET',
+            success: function(data){
+                $('.page-upload').html(data);
+            }
+        })
+    })
 })
