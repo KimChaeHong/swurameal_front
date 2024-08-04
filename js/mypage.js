9d28453eceb0686f5fab3df1e3c566fe7ad1f347
Jeong-Wonseok <jws4858@gmail.com>
2024-08-04 23:07:19 +0900

    ✅ Add : "마이페이지 멤버쉽 유무 화면전환"
    마이페이지 멤버쉽 회원일 때와 비회원일때 다른 화면 보여주는 기능 추가

--- js/mypage.js
+++ js/mypage.js
@@ -1,14 +1,49 @@
-$(document).ready(function() {
-    $('a').on('click', function(e){
-        e.preventDefault();
-        let url = $(this).attr('href');
+const user = {
+    username: "testuser",
+    email: "testuser@example.com",
+    isLoggedIn: true,
+    isMembership: true,
+    couponNum: 2,
+    pointNum: 2100,
+};
 
+$(document).ready(function () {
+    localStorage.setItem("user", JSON.stringify(user));
+    const userdata = JSON.parse(localStorage.getItem("user"));
+    $(".coupon-number").html(`${userdata.couponNum}장`);
+    $(".point-number").html(`${userdata.pointNum}p`);
+
+    if (userdata.isMembership) {
+        $(".mypage-name").html(
+            /*html*/
+            `반가워요 <span>${userdata.username}</span>님(<span>멤버쉽 회원</span>)`
+        );
+        $(".mypage-name span").css("color", "#d4c3e8");
+        $(".membership-info").html(
+            /*html*/
+            `<span class="highlight-purple">수라밀 멤버쉽</span>회원. 구매금액 <span class="highlight">5%</span> 
+            추가적립 <span class="highlight">적용중</span>`
+        );
+    }
+
+    $(".category-detail a").on("click", function (e) {
+        e.preventDefault();
+        $(".title").html(`${$(this).text()}`);
+        $(".category-detail a").css({
+            color: "#757575",
+            "font-weight": "normal",
+        });
+        let url = $(this).attr("href");
+        $(this).css({
+            "font-weight": "bold",
+            color: "black",
+        });
         $.ajax({
-            url:url,
-            method: 'GET',
-            success: function(data){
-                $('.page-upload').html(data);
-            }
-        })
-    })
-})
+            url: url,
+            method: "GET",
+            success: function (data) {
+                $(".page-upload").html(data);
+            },
+        });
+    });
+});
