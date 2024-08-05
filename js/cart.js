1381fdf1dd9984e7d08eb8b1af4f73c57bd243e7
PARK JEEAE <93249652+jiiiiiaiiiii@users.noreply.github.com>
2024-08-05 20:39:58 +0900

    ✅ Update: "장바구니 페이지 일부 수정"
    - 아이템 수량 조절 함수(리팩토링 필요)

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,7 @@ let checkBtn = document.querySelectorAll("i");
 let selfCheckBtn = document.querySelectorAll("i:not(#allBtn)");
 
 /* 전체선택 || 전체해제 */
+
 function choiceAll() {
   checked = !checked; // 체크상태 토글
 
@@ -15,6 +16,7 @@ function choiceAll() {
 }
 
 /* 개별아이템 선택 || 해제 */
+
 selfCheckBtn.forEach((el) =>
   el.addEventListener("click", (e) => {
     let icon = e.target;
@@ -25,12 +27,59 @@ selfCheckBtn.forEach((el) =>
 );
 
 /* 수량 증가 || 감소 */
-let cnt = document.querySelector(".one").innerText;
 
-function increse() {
-  cnt = parseInt(cnt) + 1;
+// const itemCnt = document.querySelector(".one");
+// let cnt = parseInt(itemCnt.innerText);
+
+// const itemPrice = document.querySelector(".one-price");
+// const itemSalePrice = document.querySelector(".one-price-sale");
+
+// let price = parseInt(itemPrice.innerText);
+// let salePrice = parseInt(itemSalePrice.innerText);
+
+// function increse() {
+//   cnt += 1;
+//   itemCnt.innerText = cnt;
+//   price = price * 2;
+//   salePrice = salePrice * 2;
+//   itemPrice.innerText = price;
+//   itemSalePrice.innerText = salePrice;
+// }
+
+// function decrese() {
+//   if (cnt > 1) {
+//     cnt -= 1;
+//     itemCnt.innerText = cnt;
+//     price = price / 2;
+//     salePrice = salePrice / 2;
+//     itemPrice.innerText = price;
+//     itemSalePrice.innerText = salePrice;
+//   }
+// }
+
+const cntBox = document.querySelector(".cnt-btn");
+function changeCnt(e) {
+  let cnt = e.target;
 }
 
-function decrese() {
-  cnt = parseInt(cnt) - 1;
+const itemCountElement = document.getElementById("item-count");
+const itemPriceElement = document.getElementById("item-price-regular");
+const itemSalePriceElement = document.getElementById("item-price-sale");
+
+let itemCount = parseInt(itemCountElement.innerText, 10);
+let itemPrice = parseInt(itemPriceElement.innerText, 10);
+let itemSalePrice = parseInt(itemSalePriceElement.innerText, 10);
+
+function updatePrices(count) {
+  itemPriceElement.innerText = itemPrice * count;
+  itemSalePriceElement.innerText = itemSalePrice * count;
+}
+
+function changeCnt(num) {
+  const newCount = itemCount + num;
+  if (newCount >= 1) {
+    itemCount = newCount;
+    itemCountElement.innerText = itemCount;
+    updatePrices(itemCount);
+  }
 }
