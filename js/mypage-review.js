1854e36328af004e6c8cafab5790c41f9af27b5f
361_seok <jws4858@gmail.com>
2024-08-05 12:19:30 +0900

    ✅ Add : "작성한 리뷰 페이지 리뷰 만큼 불러오기 기능 추가"
    localStorage에 있는 리뷰 수 만큼 불러오는 기능 추가

--- js/mypage-review.js
+++ js/mypage-review.js
@@ -4,13 +4,25 @@ let product = [
     {img : "../images/212.jpg", title: "파스타", orderNumber: "222222222", 
         paymentMethod: "신용카드", price: "7,300 원",  orderStatus: "배송 중"}
 ]
+let review = [
+    {img : "../images/211.jpg", title: "떡볶이", category: "[떡볶이] 떡볶이", 
+        star: "★★★★★", scope: "5",  description: "감사위원은 원장의 제청으로 대통령이 임명하고, 그 임기는 4년으로 하며, 1차에 한하여 중임할 수 있다. 전직대통령의 신분과 예우에 관하여는 법률로 정한다. 이 헌법공포 당시의 국회의원의 임기는 제1항에 의한 국회의 최초의 집회일 전일까지로 한다. 국군의 조직과 편성은 법률로 정한다. "},
+    {img : "../images/212.jpg", title: "파스타", category: "[파스타] 파스타", 
+        star: "★★★★☆", scope: "4",  description: "매우 맛있습니다."},
+    {img : "../images/212.jpg", title: "파스타", category: "[파스타] 파스타", 
+        star: "★★★★☆", scope: "4",  description: "매우 맛있습니다."}
+]
 
 function productUpload() {
     localStorage.setItem("product", JSON.stringify(product));
 }
 
+function reviewUpload(){
+    localStorage.setItem("review", JSON.stringify(review));
+}
+
 $(document).ready(function () {
-    
+    reviewUpload();
     productUpload();
     $(".review-writeable-box").on("click", function (e) {
         $(".review-writeable-page").html("");
@@ -34,7 +46,32 @@ $(document).ready(function () {
     });
 
     $(".review-written-box").on("click", function (e) {
+        $(".review-written-page").html("");
         $(".review-writeable-page").css("display", "none");
         $(".review-written-page").css("display", "block");
+        const reviewdata = JSON.parse(localStorage.getItem("review"));
+        for( r of reviewdata){
+            const reviewHtml = /*html*/
+            `<div class="review-box">
+            <button class="review-delete">&times;</button>
+            <div class="review-title">
+                <p class="title"><strong>${r.title}</strong></p>
+                <p class="category">${r.category}</p>
+                <p><span class="star">${r.star}</span><span class="scope"> ${r.scope}</span></p>
+            </div>
+            <div class="review-detail">
+                <div class="review-description">
+                    <p class="description">${r.description}</p>
+                </div>
+                <div class="review-image">
+                    <img src=${r.img}>
+                </div>
+                <div class="review-update">
+                    <button>수정하기</button>
+                </div>
+            </div>
+        </div>`
+            $(".review-written-page").append(reviewHtml);
+        }
     });
 });
