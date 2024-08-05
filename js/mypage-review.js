2a6d2799d04b75c4ce89b88750c21b4f478ee67f
361_seok <jws4858@gmail.com>
2024-08-05 10:59:02 +0900

    ✅ Update : "작성한 리뷰 페이지 수정"
    작성한 리뷰 페이지 위치 조정 및 버튼 추가

--- js/mypage-review.js
+++ js/mypage-review.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    console.log("ddd");
-    $(".review-writeable").on("click", function (e) {
-        console.log("aaaaaa");
+    
+    $(".review-writeable-box").on("click", function (e) {
         $(".review-written-page").css("display", "none");
         $(".review-writeable-page").css("display", "block");
     });
 
-    $(".review-written").on("click", function (e) {
-        console.log("sfasdf");
+    $(".review-written-box").on("click", function (e) {
         $(".review-writeable-page").css("display", "none");
         $(".review-written-page").css("display", "block");
     });
