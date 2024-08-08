1b19b925c69a0e10dba5d189da7ecda1881e0225
kimccchong <49267413+KimChaeHong@users.noreply.github.com>
2024-08-08 11:15:56 +0900

    Delete js directory

--- js/faqPage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.clickable').click(function() {
-        var contentId = $(this).data('content');
-        $('.dropdown-box').not('#' + contentId).slideUp();
-        $('#' + contentId).slideToggle();
-    });
-});
--- js/membership.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('sub-button').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'block' ;
-    // document.getElementById('popup').classList.add('popup-visible');
-});
-
-document.getElementById('close-button').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none' ;
-});
