2cda2f01eddd93c57d19eaf32067cdedc73209ff
PARK JEEAE <msp827@naver.com>
2024-08-12 17:39:23 +0900

    ✅ Add : "login 페이지 로그인 id/pw 추가"
    - ID: swura4789
    - PW: mealkit@123
    (로그인 후의 header, nav는 추후 구현 예정)

--- js/login.js
+++ js/login.js
@@ -1,11 +1,22 @@
+/* 회원가입 페이지로 이동 */
 function toSignup() {
-	window.location.href = "/html/signUp.html";
+  window.location.href = "/html/signUp.html";
 }
 
-/* 페이지 이동 url 생성 */
+/* id/pw 찾기 페이지로 이동 */
 function findIdPw(type) {
   const baseUrl = "/html/find.html"; // 페이지를 이동 URL 생성
   const url = `${baseUrl}?type=${type}`;
   window.location.href = url; // 페이지 이동
 }
 
+/* 로그인 처리 */
+function loginCheck() {
+  if ($("#id").val() != "swura4789" || $("#pw").val() != "mealkit@123") {
+    console.log($("#id").val());
+    console.log($("#pw").val());
+    alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } else {
+    window.location.href = "../../index.html";
+  }
+}
