67dc164db147d5f061bb3233a791044186a4b9d4
361_seok <jws4858@gmail.com>
2024-08-14 16:33:07 +0900

    ✅ Update: "공지사항 등록/수정에 따른 페이지 변경 완료"
    공지사항 등록은 등록버튼
    공지사항 수정은 수정/삭제버튼으로 수정

--- js/notice-form.js
+++ js/notice-form.js
@@ -1,28 +1,43 @@
 function uploadNoticeFormPage(noticeInfo) {
+    $(".addButton button").remove();
+    /*prettier-ignore*/
     const noticeFormHtml =
         /*html*/
         `
-    <div class="notice-form d-flex flex-column">
-        <div>
-            <p>제목<span>*</span></p>
-            <div class="notice-form-title"></div>
+        <div class="notice-form d-flex flex-column">
+            <div class="notice-title-box">
+                <p>제목<span>*</span></p>
+                <input type="text" value="${isDataCheck(noticeInfo?.title)}" class="notice-form-title" placeholder="제목을 입력해주세요.">
+            </div>
+            <div class="notice-content-box">
+                <p>내용<span>*</span></p>
+                <textarea class="notice-form-content" placeholder="내용을 입력해주세요.">${isDataCheck(noticeInfo?.description)}</textarea>
+            </div>
+            <div class="notice-form-buttons">
+                <button >삭제</button>
+                <button>등록</button>
+            </div>
         </div>
-        <div>
-            <p>내용<span>*</span></p>
-            <div class="notice-form-content"></div>
-        </div>
-        <div class="notice-form-buttons">
-            <button>삭제</button>
-            <button>등록</button>
-        </div>
-    </div>
-    `;
-    $(".page-upload").html(noticeFormHtml);
+        `;
 
+    $(".page-upload").html(noticeFormHtml);
     const noticeFormCss = $("<link>", {
         rel: "stylesheet",
         type: "text/css",
         href: "../css/notice-form.css",
     });
-    $(head).append(noticeFormCss);
+    $("head").append(noticeFormCss);
+
+    if (noticeInfo != undefined) {
+        console.log($(".notice-form-buttons :nth-child(2)").text());
+        $(".notice-form-buttons :nth-child(1)").css("display", "block");
+        $(".notice-form-buttons :nth-child(2)").text("수정");
+    } else {
+        $(".notice-form-buttons :nth-child(2)").text("등록");
+    }
+}
+
+function isDataCheck(infoData) {
+    console.log(infoData);
+    return infoData || "";
 }
