4401c74d2351a93a7133c8500e1465bb62fb2d44
junbum6694 <wnsqja6694@gmail.com>
2024-08-15 18:50:29 +0900

    ✅ Update: "찜버튼 기능 추가, 주석 추가"
    찜버튼 기능 추가, 주석 추가

--- js/detail.js
+++ js/detail.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     $("#product-price").text(price.toLocaleString() + "원");
     var totalPrice = Number($("#count").text()) * price;
 
-    $("#minus-button").on("click", function () {
+    $("#minus-button").on("click", function () { //상품개수 감소버튼
         if (Number($("#count").text()) <= 1) {
         } else {
             var num = Number($("#count").text()) - 1;
@@ -29,7 +29,7 @@ $(document).ready(function () {
         }
     });
 
-    $("#plus-button").on("click", function () {
+    $("#plus-button").on("click", function () { //상품개수 증가버튼
         var num = Number($("#count").text()) + 1;
         $("#count").text(num);
         totalPrice = Number($("#count").text()) * price;
@@ -44,26 +44,26 @@ $(document).ready(function () {
         }
     });
 
-    $("#move-info").on("click", function () {
+    $("#move-info").on("click", function () { //제품정보 버튼
         $("#detail-img1")[0].scrollIntoView();
     });
 
-    $("#move-detail").on("click", function () {
+    $("#move-detail").on("click", function () { //제품상세 버튼
         $("#detail-img2")[0].scrollIntoView();
     });
 
-    $("#move-review").on("click", function () {
+    $("#move-review").on("click", function () { // 리뷰 버튼
         $("#review-container")[0].scrollIntoView();
     });
 
-    reviewUpload();
+    reviewUpload(); //리뷰 목록 호출
     var review = JSON.parse(localStorage.getItem("reviews"));
     console.log(review);
 
     const itemPerPage = 5;
     let currentPage = 1;
 
-    function displayReviews(page) {
+    function displayReviews(page) { //해당 페이지 리뷰 생성
         const startIndex = (page - 1) * itemPerPage;
         const endIndex = startIndex + itemPerPage;
         currentPageReviews = review.slice(startIndex, endIndex);
@@ -85,7 +85,7 @@ $(document).ready(function () {
         }
     }
 
-    function setupPagination() {
+    function setupPagination() { //페이지 번호 생성
         const pageCount = Math.ceil(review.length / itemPerPage);
         $(".pagination").empty();
 
@@ -101,7 +101,7 @@ $(document).ready(function () {
     displayReviews(currentPage);
     setupPagination();
 
-    $(".pagination").on("click", ".page-link", function (e) {
+    $(".pagination").on("click", ".page-link", function (e) { //페이지 번호 이벤트
         e.preventDefault();
         currentPage = Number($(this).text());
         displayReviews(currentPage);
@@ -110,8 +110,24 @@ $(document).ready(function () {
 
     $('#count-review').text(review.length+'건');
 
-    $(".to-cart, #cart").on('click', function() {
+    $(".to-cart, #cart").on('click', function() { // 장바구니 버튼기능
         window.alert("상품을 장바구니에 추가하였습니다.");
     })
+
+    $(document).on('click','#pick', function() { //찜버튼 기능
+        className = $('#pick-icon').attr('class');
+        console.log(className);
+
+        if(className =="bi bi-heart"){
+            $('#pick-icon').removeClass('bi-heart');
+            $('#pick-icon').addClass('bi-heart-fill');
+            alert('찜한상품에 추가되었습니다.');
+        }else {
+            $('#pick-icon').removeClass('bi bi-heart-fill');
+            $('#pick-icon').addClass('bi-heart');
+            alert('찜한상품에서 제거되었습니다.');
+        }
+
+    });
 
 });
